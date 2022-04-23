9a269e09399097b3e22a41fb2a4cdffeb66aa5a1
M4ss1ck <4ndyraul@gmail.com>
2022-04-23 07:13:30 -0400

    ready to get rid of css modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./js/pages/app.jsx";
 
-render(<App />, document.querySelector("#app"));
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
