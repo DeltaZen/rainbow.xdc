24d8ce98b84e4c596f6abd09a81cc81f6ba64bac
M4ss1ck <4ndyraul@gmail.com>
2022-04-23 03:58:30 -0400

    installing webpack stuff

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,5 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './js/pages/app.jsx'
+import React from "react";
+import { render } from "react-dom";
+import App from "./js/pages/app.jsx";
 
-render(<AppContainer><App /></AppContainer>, document.querySelector('#app'))
-
-if (module && module.hot) {
-  module.hot.accept('./js/pages/app.jsx', () => {
-    const App = require('./js/pages/app.jsx').default
-    render(
-      <AppContainer>
-        <App />
-      </AppContainer>,
-      document.querySelector('#app')
-    )
-  })
-}
+render(<App />, document.getElementById("app"));
