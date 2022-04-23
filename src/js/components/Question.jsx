368fbe8d9cc76601db47b3c0aaf9e46f050812a2
M4ss1ck <4ndyraul@gmail.com>
2022-04-23 04:35:00 -0400

    fixing warnings and errors

--- src/js/components/Question.jsx
+++ src/js/components/Question.jsx
@@ -1,64 +1,71 @@
-import questionCss from '../../css/components/Question.scss'
-import Countdown from './Countdown'
-import React, { Component } from 'react'
+import questionCss from "../../css/components/Question.scss";
+import Countdown from "./Countdown.jsx";
+import React, { Component } from "react";
 
 class Question extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       timeLeft: props.totalTime,
       addTime: props.addTime,
-      addedTime: 0
-    }
-    this.timeout = {}
+      addedTime: 0,
+    };
+    this.timeout = {};
   }
 
-  componentDidUpdate () {
-    const { addTime, onTimeout } = this.props
+  componentDidUpdate() {
+    const { addTime, onTimeout } = this.props;
     if (this.state.addTime !== addTime) {
-      let cappedTime = Math.min(Question.defaultProps.totalTime, this.state.timeLeft + addTime)
+      let cappedTime = Math.min(
+        Question.defaultProps.totalTime,
+        this.state.timeLeft + addTime
+      );
       this.setState({
         addTime,
-        timeLeft: cappedTime
-      })
+        timeLeft: cappedTime,
+      });
     }
     if (this.state.timeLeft <= 0) {
-      onTimeout()
+      onTimeout();
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const loop = () => {
-      if (this.state.timeLeft <= 0) return
-      this.setState({ timeLeft: this.state.timeLeft - 100 })
-      this.timeout = setTimeout(loop, 100)
-    }
-    loop()
+      if (this.state.timeLeft <= 0) return;
+      this.setState({ timeLeft: this.state.timeLeft - 100 });
+      this.timeout = setTimeout(loop, 100);
+    };
+    loop();
   }
 
-  componentWillUnmount () {
-    clearTimeout(this.timeout)
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
   }
 
-  render () {
-    const { word, colour, readColourRound, totalTime } = this.props
+  render() {
+    const { word, colour, readColourRound, totalTime } = this.props;
     return (
       <section className={questionCss.card}>
-        {readColourRound
-          ? (<span className={questionCss.highlight}>📖 read</span>)
-          : (<span className={questionCss.highlightAlt}>see 👀</span>)}
+        {readColourRound ? (
+          <span className={questionCss.highlight}>📖 read</span>
+        ) : (
+          <span className={questionCss.highlightAlt}>see 👀</span>
+        )}
         <Countdown timeLeft={this.state.timeLeft} totalTime={totalTime} />
-        <h2 className={`${questionCss[colour]} ${questionCss.question}`}>{word}</h2>
+        <h2 className={`${questionCss[colour]} ${questionCss.question}`}>
+          {word}
+        </h2>
       </section>
-    )
+    );
   }
 }
 
 Question.defaultProps = {
   totalTime: 10000,
   timeLeft: 10000,
-  addTime: 0
-}
+  addTime: 0,
+};
 
-export default Question
+export default Question;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   entry: path.join(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -17,6 +18,17 @@ module.exports = {
             presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
