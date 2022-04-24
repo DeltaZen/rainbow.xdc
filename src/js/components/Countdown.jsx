bb014d5c089b3ab1a8fc19c2fe01921eec70e6c2
M4ss1ck <4ndyraul@gmail.com>
2022-04-24 05:42:27 -0400

    fix countdown

--- src/js/components/Countdown.jsx
+++ src/js/components/Countdown.jsx
@@ -1,13 +1,11 @@
-import countdownCss from '../../css/components/Countdown.scss'
-import React from 'react'
+import "../../css/components/Countdown.scss";
+import React from "react";
 
-export default ({timeLeft, totalTime}) => {
-  timeLeft = totalTime ? timeLeft / totalTime : 0
+export default ({ timeLeft, totalTime }) => {
+  timeLeft = totalTime ? timeLeft / totalTime : 0;
   return (
-    <div className={countdownCss.countdown}>
-      <div className={countdownCss.remaining}
-        style={{ width: `${timeLeft * 100}%` }}
-      />
+    <div className="countdown">
+      <div className="remaining" style={{ width: `${timeLeft * 100}%` }} />
     </div>
-  )
-}
+  );
+};
